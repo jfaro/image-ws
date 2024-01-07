15f51bc8148013a53cc0bc7336e01e3106619a91
Julien Faro <jnfaro99@gmail.com>
2024-01-07 13:06:30 -0800

    Add initial Websocket useEffect impl in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,35 @@
 import './App.css'
+import { useEffect, useRef } from 'react';
+
+const URL = "ws://127.0.0.1:8080"
 
 function App() {
+  const connection = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket(URL);
+
+    // Connection opened.
+    socket.addEventListener("open", (event) => {
+      console.log("New connection opened", event)
+      socket.send("Connection established")
+    })
+
+    // Listen for messages.
+    socket.addEventListener("message", (event) => {
+      console.log("Message from server", event.data)
+    })
+
+    // Save ref to connection.
+    connection.current = socket;
+
+    // Close connection on unmount.
+    return () => socket.close()
+  }, [])
+
   return (
     <>
       <div>
-        Hello
       </div>
     </>
   )
