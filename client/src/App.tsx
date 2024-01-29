7312e0d8fea6ed1b83a0872e2bc35075a96816f1
Julien Faro <jnfaro99@gmail.com>
2024-01-28 19:23:35 -0800

    Add auto-reconnecto to ws hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,80 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css'
-import { useEffect, useRef } from 'react';
 
-const URL = "ws://127.0.0.1:8080/ws"
+const SOCKET_URL = "ws://127.0.0.1:8080/ws"
+const SOCKET_RECONNECT_INTERVAL = 1000;
 
-function App() {
-  const connection = useRef<WebSocket | null>(null);
+enum Status {
+  Init = "initializing",
+  Open = "open",
+  Closed = "closed",
+}
+
+const useSocket = () => {
+  const ws = useRef<WebSocket>();
+  // const [ws, setWs] = useState<WebSocket>();
+  const [status, setStatus] = useState(Status.Closed)
+
+  // Handle socket open.
+  const onOpen = useCallback(() => {
+    console.log("Connection established")
+    setStatus(Status.Open)
+  }, [])
+
+  // Handle socket close.
+  const onClose = useCallback(() => {
+    console.log("Connection closed")
+    setStatus(Status.Closed);
+    ws.current = undefined
+  }, [])
+
+  // Handle socket message.
+  const onMessage = useCallback((event: MessageEvent<any>) => {
+    console.log("Message received on socket", event.data)
+  }, []);
+
+  // Create new web socket and register event handlers.
+  const initSocket = useCallback(() => {
+    console.log(`Creating connection at ${SOCKET_URL}`)
+    const socket = new WebSocket(SOCKET_URL);
+    socket.addEventListener("open", onOpen);
+    socket.addEventListener("close", onClose);
+    socket.addEventListener("message", onMessage)
+    ws.current = socket;
+  }, []);
 
   useEffect(() => {
-    const socket = new WebSocket(URL);
+    let interval = setInterval(() => {
+      // Clear closed connection.
+      if (ws.current && ws.current.readyState === ws.current.CLOSED) {
+        console.log("Removing closed connection")
+        ws.current = undefined;
+      }
 
-    // Connection opened.
-    socket.addEventListener("open", (event) => {
-      console.log("New connection opened", event)
-      socket.send("Connection established")
-    })
+      // Initialize connection.
+      if (!ws.current) {
+        initSocket()
+      }
+    }, SOCKET_RECONNECT_INTERVAL)
 
-    // Listen for messages.
-    socket.addEventListener("message", (event) => {
-      console.log("Message from server", event.data)
-    })
+    return () => {
+      clearInterval(interval)
+      ws.current?.removeEventListener("open", (_) => onOpen())
+      ws.current?.removeEventListener("close", onClose)
+      ws.current?.removeEventListener("message", onMessage)
+    }
+  })
 
-    // Save ref to connection.
-    connection.current = socket;
+  return { status }
+}
 
-    // Close connection on unmount.
-    return () => socket.close()
-  }, [])
+function App() {
+  const { status } = useSocket();
 
   return (
     <>
       <div>
+        Connection: {status}
       </div>
     </>
   )
